89ace75a79a86f051f790b69303fc21635fe2341
MDsona <MD-S2013@hotmail.com>
2019-12-24 01:40:48 +0300

    installed virtual enviroment

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\softwa~1\learning\django~1\blogger\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- src/my_blog/settings.py
+++ src/my_blog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',             # -1a
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
