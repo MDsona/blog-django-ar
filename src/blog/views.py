dd0c105365245ce14c9abc3fd4b072b882250e1c
MDsona <MD-S2013@hotmail.com>
2019-12-25 23:17:05 +0300

    add post content (views,index)

--- src/blog/views.py
+++ src/blog/views.py
@@ -2,6 +2,31 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):                                                  # -2b
-    return render(request, 'blog/index.html', {'title': 'home'})
+posts = [                                                         # -3a
+    {
+        'title': 'التدوينة الاولى',
+        'content': 'نص التدوينة نـــصــ تـجريبيـــ',
+        'post_date': '15-12-2019',
+        'author': 'MDsona'
+    },
+    {
+        'title': 'التدوينة الاولى',
+        'content': 'نص التدوينة نـــصــ تـجريبيـــ',
+        'post_date': '15-12-2019',
+        'author': 'olom web'
+    },
+    {
+        'title': 'التدوينة الاولى',
+        'content': 'نص التدوينة نـــصــ تـجريبيـــ',
+        'post_date': '15-12-2019',
+        'author': 'olom web'
+    }
+]
+
+def home(request):                                                  # -2b {'title': 'home'}
+    context = {                                                     # -3a
+        'title': 'الصفحة الرئيسية',
+        'posts': posts
+    }
+    return render(request, 'blog/index.html', context)
 
