ced3ecc80bccb0801babd5fc4bed3cf8f1ae7ee6
MDsona <MD-S2013@hotmail.com>
2020-01-10 14:33:56 +0300

    pagination for blog/index

--- src/blog/views.py
+++ src/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404          # -7b : add 404
 
 from .models import Post, Comment                              # -6a, -8d
 from .forms import NewComment                                   # -9b
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage  # - 24a
 
 # Create your views here.
 
@@ -27,9 +28,20 @@ from .forms import NewComment                                   # -9b
 # ]
 
 def home(request):                                                  # -2b {'title': 'home'}
+    posts = Post.objects.all()                                      # -24a
+    paginator = Paginator(posts, 5)
+    page = request.GET.get('page')
+    try:                                                            # -24a
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_page)
+
     context = {                                                     # -3a
         'page_title': 'الصفحة الرئيسية',
-        'posts': Post.objects.all()                                 # -6a
+        'posts': posts,                                              # -6a, 24a
+        'page': page,                                               # -24b
     }
     return render(request, 'blog/index.html', context)
 
--- src/user/models.py
+++ src/user/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 from django.contrib.auth.models import User                                     # -20a
 from django.db.models.signals import post_save                                 # -21a
+from PIL import Image                                                           # -23a pillow
 
 
 class Profile(models.Model):                                                    # -20a
@@ -12,6 +13,15 @@ class Profile(models.Model):                                                    
 
     def __str__(self):                                                          # -20e
         return f'{self.user.username} profile.'
+
+    def save(self, *args, **kwargs):                          # -23a, 23b(args, kwargs)
+        super().save(*args, **kwargs)                               # , 23b
+
+        img = Image.open(self.image.path)
+        if img.width > 300 or img.height > 300:
+            output_size = (300, 300)                # 300X300 px
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
 
 def create_profile(sender, **kwarg):                                            # -21a
